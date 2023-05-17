d16dc554d8642dfb30a8c5872aa9ca38519a4edd
Vedant Patel <vedant.patel@BLRETV-330GMD6M.local>
2023-05-17 17:32:12 +0530

    first java code

---
+++ src/com/flipkart/demo/Demo.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * @author vedant.patel
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is my first programme");
+
+	}
+
+}
